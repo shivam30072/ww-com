f01e2481c14c9276877b504ccf9abaf59d0b89ad
Siddharthmishraweb <mishrasiddharth1999@gmail.com>
2024-12-31 10:14:45 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   env: {
-    BACKEND_BASE_URL: "http://localhost:4847",
+    BACKEND_BASE_URL: "https://siya-backend-1.onrender.com",
     USER_ID: '672f138c6090c2215c603e4e'
   },
 };
